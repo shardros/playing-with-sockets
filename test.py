93c841e57b79d430e0f1de7e7d959910d2dbd07d
edwin <Edwin Shepherd>
2020-05-11 17:35:27 +0100

    socketio

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import threading
 import copy
 
 from flask import Flask, render_template, Response
-from flask_socketio import SocketIO, emit
 from PIL import Image, ImageDraw
 from io import BytesIO, StringIO
 
@@ -15,7 +14,6 @@ class ImageSource(object):
         self.image_lock = threading.Lock()
         self.image = None
         self.i = 0
-        self.new_image = threading.Event()
         t = threading.Thread(target=self.generate_new_images)
         t.start()
 
@@ -34,26 +32,22 @@ class ImageSource(object):
             with self.image_lock:
                 self.image = img_io.read()
             print("Updated image")
-            self.new_image.set()
             time.sleep(10)
 
     def get_frame(self):
         while True:
-            if self.new_image.wait(1):
-                with self.image_lock:
-                    frame = self.image
+            with self.image_lock:
+                frame = self.image
 
-                    if frame is None:
-                        continue
+                if frame is None:
+                    continue
 
-                    emit("new-frame", frame)
-            # TODO check for shutdown
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 app = Flask(__name__)
 image_source = ImageSource()
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -63,16 +57,10 @@ def index():
 def video_feed():
     res = Response(image_source.get_frame(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+    print(res)
     return res
 
-@socketio.on('message')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)@socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
-
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
-    print(image_source.get_frame())
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
